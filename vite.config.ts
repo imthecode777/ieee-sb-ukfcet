97107fea7c107ee9886f2f159276dbb2703921bf
Fahad Siraj <fahadsiraj202281@gmail.com>
2025-03-15 20:42:01 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-
-  base: "/ieee-sb-ukfcet",
+  base: mode === "production" ? "/ieee-sb-ukfcet/" : "/",
 }));
